0c54ac6bb901d3689c0586d8ae6cb3fafb6b246d
Harini0610 <115541071+Harini0610@users.noreply.github.com>
2025-03-02 16:08:58 +0530

    Update Basic.java

--- Basic.java
+++ Basic.java
@@ -4,6 +4,10 @@ public class Basic
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("HELLO WORLD!!!");
+		System.out.println("enter two values:");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println("sum="+(a+b));
+		
 	}
 }
